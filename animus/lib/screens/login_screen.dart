49ae1e4e6ed5be0a3d36daaa6c6fcccbce3216c7
LuisFernandoH <luisspatan10@gmail.com>
2024-11-05 19:27:49 -0600

    Cambios en el diseño

--- animus/lib/screens/login_screen.dart
+++ animus/lib/screens/login_screen.dart
@@ -4,13 +4,20 @@ import 'package:animus/services/auth_services.dart';
 import 'package:animus/services/notifications_services.dart';
 import 'package:provider/provider.dart';
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black87, Colors.black54],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
         child: ChangeNotifierProvider(
           create: (_) => LoginFormProvider(),
           child: _LoginForm(),
@@ -29,139 +36,159 @@ class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
-    //creamos instancia del loginfromprovider que ya tenemos hecho
     final loginForm = Provider.of<LoginFormProvider>(context);
 
     return Center(
-      child: Scaffold(
-        body: DecoratedBox(
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 109, 104, 128)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 16, ),
-                Container(
-                  width: size.width * 0.80,
-                  height: size.height * 0.17,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                    //todo: poner image
-                    //image: decorationimage(image: ...)
-                    //es por si va una imagen en la parte de arriibita de, box
-                  ),
-                ),
-                Container(
-                  width: size.width * 0.80,
-                  height: size.height * 0.05,
-                  alignment: Alignment.center,
-                ),
-                TextFormField(
-                  autocorrect: false, //sin autocorrector pq es el correo,
-                  keyboardType: TextInputType.emailAddress,
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    hintText: 'ejemplo@cola.com',
-                    labelText: 'Email',
-                    labelStyle: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 255, 244, 244)
-                    ),
-                  ),
-                  validator: (value){
-                    String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                    RegExp regExp = RegExp(pattern);
-                    return regExp.hasMatch(value ?? '') ? null : 'El valor ingresado no es un correo';
-                  },
-                ),
-                TextFormField(
-                  autocorrect: false,
-                  controller: _passwordController,
-                  obscureText: true, //tapa loq  escribes ,
-                  decoration: const InputDecoration(
-                    hintText: '********',
-                    labelText: 'Password',
-                    labelStyle: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 253, 246, 246)
-                    )
-                  ),
-                  validator: (value){
-                    return (value != null && value.length >= 8) ? null : 'La contrasenia debe ser mayor a 7 caracteres';
-                  },
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextButton(
-                  onPressed: (){}, 
-                  child: const Text('Olvidaste tu contrasenia?', 
-                    style: TextStyle(color: Color.fromARGB(255, 255, 244, 244))
-                  ), 
-                ),
-                ElevatedButton(
-                  onPressed: loginForm.isLoading ? null : () async {
-                    final authService = Provider.of<AuthServices>(context, listen: false);
-                    //if(!loginForm.isValidForm()) return;
-
-                    final String? errorMessage = await authService.login(
-                      //loginForm.email, loginForm.password
-                      _emailController.text, _passwordController.text
-                    );
-
-                    if(errorMessage == null){
-                      Navigator.pushReplacementNamed(context, 'home');
-                    }
-                    else {
-                      NotificationsServices.showSnackbar(errorMessage);
-                      loginForm.isLoading = false;
-                    }
-                  }, 
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 181, 184, 187)
-                    )
-                  ),
-                  child: const Text(
-                    'Iniciar sesion',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 250, 253, 247)
-                    )
-                  )
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                ElevatedButton(
-                  style:  ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Color.fromARGB(255, 173, 171, 171),
-                    )
-                  ),
-                  onPressed: (){
-                    Navigator.pushReplacementNamed(context, 'register', arguments: '');
-                  },
-                  child: const Text(
-                    'Registrate',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 96, 108, 93)
-                    ),
-                  ),
-                ),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Icono o logo en la parte superior
+            const Icon(
+              Icons.shield,
+              color: Colors.red,
+              size: 80,
             ),
-          ),
+            const SizedBox(height: 20),
+            const Text(
+              'Inicia Sesión',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              controller: _emailController,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'ejemplo@correo.com',
+                labelText: 'Email',
+                labelStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+                hintStyle: const TextStyle(color: Colors.white54),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.redAccent),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              validator: (value) {
+                String pattern =
+                    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
+                return RegExp(pattern).hasMatch(value ?? '')
+                    ? null
+                    : 'Correo no válido';
+              },
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: '********',
+                labelText: 'Password',
+                labelStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+                hintStyle: const TextStyle(color: Colors.white54),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.redAccent),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.red),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              validator: (value) {
+                return (value != null && value.length >= 8)
+                    ? null
+                    : 'Debe tener al menos 8 caracteres';
+              },
+            ),
+            const SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  '¿Olvidaste tu contraseña?',
+                  style: TextStyle(color: Colors.redAccent),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: loginForm.isLoading
+                  ? null
+                  : () async {
+                      final authService =
+                          Provider.of<AuthServices>(context, listen: false);
+                      final String? errorMessage = await authService.login(
+                        _emailController.text,
+                        _passwordController.text,
+                      );
+
+                      if (errorMessage == null) {
+                        Navigator.pushReplacementNamed(context, 'home');
+                      } else {
+                        NotificationsServices.showSnackbar(errorMessage);
+                        loginForm.isLoading = false;
+                      }
+                    },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Iniciar sesión',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, 'register');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[800],
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Regístrate',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- animus/lib/screens/registro_screen.dart
+++ animus/lib/screens/registro_screen.dart
@@ -4,137 +4,152 @@ import 'package:animus/services/auth_services.dart';
 import 'package:animus/services/notifications_services.dart';
 import 'package:provider/provider.dart';
 
-class RegistroScreen extends StatelessWidget{
+class RegistroScreen extends StatelessWidget {
   RegistroScreen({super.key});
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-
   @override
-  Widget build(BuildContext context){
-    //final loginForm = Provider.of<LoginFormProvider>(context);
+  Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-
     return Scaffold(
-      body: DecoratedBox(
+      body: Container(
+        width: double.infinity,
         decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 192, 131, 51)
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF1E1E1E), // color oscuro de fondo
+              Color(0xFF444444), // gradiente gris medio
+              Color(0xFF8C8C8C), // gradiente gris claro
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              // Logo o Imagen Decorativa
               Container(
-                width: size.width * 0.80,
-                height: size.height* 0.17,
-                alignment: Alignment.center,
+                width: size.width * 0.6,
+                height: size.height * 0.2,
                 decoration: const BoxDecoration(
-                  //todo: agregar una imagen
+                  image: DecorationImage(
+                    image: AssetImage('../assets/images/assassins_creed_logo.png'), // Coloca el logo en tu carpeta de assets
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
-              Container(
-                width: size.width * 0.80,
-                height: size.height* 0.05,
-                alignment: Alignment.center,
-              ),
+              const SizedBox(height: 40),
+              // Campo de Email
               TextField(
-                controller:  _emailController,
+                controller: _emailController,
                 decoration: const InputDecoration(
                   hintText: 'ejemplo@email.com',
                   labelText: 'Email',
                   labelStyle: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 255, 244, 244)
-                  )
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromARGB(255, 255, 244, 244),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  filled: true,
+                  fillColor: Color(0xFF303030),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
+                style: const TextStyle(color: Colors.white),
               ),
-              const SizedBox(
-                height: 16,
-              ),
+              const SizedBox(height: 20),
+              // Campo de Contraseña
               TextField(
-                controller:  _passwordController,
+                controller: _passwordController,
                 obscureText: true,
                 decoration: const InputDecoration(
                   hintText: '*****',
-                  labelText: 'Contrasenia',
+                  labelText: 'Contraseña',
                   labelStyle: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 255, 244, 244)
-                  )
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromARGB(255, 255, 244, 244),
+                  ),
+                  hintStyle: TextStyle(color: Colors.grey),
+                  filled: true,
+                  fillColor: Color(0xFF303030),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+                style: const TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              // Botón Olvidaste tu contraseña
+              TextButton(
+                onPressed: () {
+                  // Acción de recuperación de contraseña
+                },
+                child: const Text(
+                  '¿Olvidaste tu contraseña?',
+                  style: TextStyle(color: Color.fromARGB(255, 255, 244, 244)),
                 ),
               ),
-              const SizedBox(
-                  height: 16,
-                ),
-                TextButton(
-                  onPressed: (){}, 
-                  child: const Text('Olvidaste tu contrasenia?', 
-                    style: TextStyle(color: Color.fromARGB(255, 255, 244, 244))
-                  ), 
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    final authService = Provider.of<AuthServices>(context, listen: false);
+              // Botón de Registro
+              ElevatedButton(
+                onPressed: () async {
+                  final authService = Provider.of<AuthServices>(context, listen: false);
+                  final String? errorMessage = await authService.createUser(
+                    _emailController.text,
+                    _passwordController.text,
+                  );
 
-                    final String? errorMessage = await authService.createUser(
-                      //loginForm.email, loginForm.password
-                      _emailController.text, _passwordController.text
-                    );
-
-                    if(errorMessage == null){
-                      Navigator.pushReplacementNamed(context, 'login');
-                    }
-                    else {
-                      NotificationsServices.showSnackbar(errorMessage);
-                    }
-                  }, 
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                    Color.fromARGB(255, 181, 184, 187
-                    )
-                  )
+                  if (errorMessage == null) {
+                    Navigator.pushReplacementNamed(context, 'login');
+                  } else {
+                    NotificationsServices.showSnackbar(errorMessage);
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color.fromARGB(255, 181, 184, 187),
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
                 ),
                 child: const Text(
                   'Registra tu usuario',
                   style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w500,
-                    color: Color.fromARGB(255, 250, 253, 247)
-                  )
-                )
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextButton(
-                onPressed: (){
-                  print('Botin iniciar sesion');
-                  Navigator.pushNamed(context, 'login', arguments: '');
-                }, 
-                child: const Text(
-                  'Ya tienes una cuenta? Iniciar sesion', 
-                  style: TextStyle(color: Color.fromARGB(255, 255, 244, 244),
-                  )
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromARGB(255, 250, 253, 247),
+                  ),
                 ),
               ),
-              // const SizedBox(
-              //   height: 16,
-              // ),
-              // ElevatedButton(
-              //   style: ButtonStyle(
-              //     backgroundColor: MaterialStateProperty.all<Color>(
-              //       Color.fromARGB(255, 255, 244, 244)
-              //     )
-              //   ),
-              //   onPressed: (){
-
-              //   },
-              // )
+              const SizedBox(height: 16),
+              // Botón para Iniciar Sesión si ya tiene cuenta
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'login');
+                },
+                child: const Text(
+                  '¿Ya tienes una cuenta? Inicia sesión',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 244, 244),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
